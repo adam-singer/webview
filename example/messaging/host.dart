b8962b5f8dcf43ee4fa6243f04c83ea202116e6f
rmsmith <ross.dart.lang@gmail.com>
2013-01-20 14:27:40 -0800

    work on the messaging host

--- example/messaging/host.dart
+++ example/messaging/host.dart
@@ -2,16 +2,32 @@
 import 'dart:html';
 import 'dart:isolate';
 import 'dart:json';
+import 'package:web_ui/watcher.dart' as watcher;
+
+bool enabled = false;
+String message = '';
 
 main() {
-  // xtag is null until 1 tick later (known dart web ui issue)
-  // contentWindow is undefined until 1 tick more (TODO: report Chrome bug)
-  // there also seems to be some initial delay beore we can post a message,
-  // we should investigate deeper and see if there is some event we can observe.
-  new Timer.repeating(1000, (t) => send()); 
+  // xtag is null until the end of the event loop (known dart web ui issue)
+  new Timer(0, (t) {
+    var webview = document.query('x-webview').xtag;  
+    webview.on['loadcommit'].add(onLoadCommit);
+  });
 }
 
-send() {
+onLoadCommit(event) {  
+  var detail = JSON.parse(event.detail);
+  if (!detail['isTopLevel']) return;
+  // Once the embedded content has finished loading, we enable message sending.
+  enabled = true;
+  // We need to manually dispatch watchers because this event listener is not
+  // bound through html template.  
+  // TODO(rms): investigate binding to custom events in web ui html templates.
+  watcher.dispatch();
+}
+
+onSubmit(event) { 
+  event.preventDefault();
   var webview = document.query('x-webview').xtag;
-  webview.contentWindow.postMessage('bonjour from the dart!', '*');
+  webview.contentWindow.postMessage(message, '*');
 }
