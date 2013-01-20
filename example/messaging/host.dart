b2730e6454b465c4f6b7f259dcf1ac3ce960e4fe
rmsmith <ross.dart.lang@gmail.com>
2013-01-20 15:03:01 -0800

    data bind the content url and TODO we know how to build this in code

--- example/messaging/host.dart
+++ example/messaging/host.dart
@@ -6,12 +6,19 @@ import 'package:web_ui/watcher.dart' as watcher;
 
 bool enabled = false;
 String message = '';
+String contentUrl = '';
 
 main() {
   // xtag is null until the end of the event loop (known dart web ui issue)
   new Timer(0, (t) {
-    var webview = document.query('x-webview').xtag;  
+    var webview = document.query('x-webview').xtag;
     webview.on['loadcommit'].add(onLoadCommit);
+    
+    // TODO: build content url of the form:
+    // 'chrome-extension://<extensionID>/<pathToFile>' as described:
+    // http://developer.chrome.com/extensions/overview.html
+    contentUrl = 'http://127.0.0.1:3030/D:/github/webview/example/messaging/out/example/messaging/peer.html';
+    watcher.dispatch();
   });
 }
 
