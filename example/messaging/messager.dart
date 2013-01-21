2956bf2e88500322f47dce0fcab2c0ac972c0771
rmsmith <ross.dart.lang@gmail.com>
2013-01-20 19:32:35 -0800

    more refactoring and cleanup in message sample

--- example/messaging/messager.dart
+++ example/messaging/messager.dart
@@ -10,6 +10,8 @@ class Messager extends WebComponent {
   bool get enabled => target != null;
   String message = '';
   
+  List received = new List();
+  
   WindowBase _target;
   WindowBase get target  => _target;
              set target(value) {
@@ -17,9 +19,20 @@ class Messager extends WebComponent {
                watcher.dispatch();
              }
   
+  void inserted() {
+    window.on.message.add((e) {       
+      var date = new Date.now();
+      
+      // TODO: parse e.data as json and extract 'sender', 'message'
+      
+      received.addLast([e.source, e.data, '${date.hour}:${date.minute}']);
+      watcher.dispatch();
+    });
+  }
+  
   onSubmit(event) { 
     event.preventDefault();
     target.postMessage(message, '*');
     message = '';
-  }  
+  }
 }
